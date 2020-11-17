0c3de4138b896a9632467442491bc30aff9c40ff
Zahnen Garner <zahnen@gmail.com>
2020-11-17 13:19:25 -0800

    separate promise logic into service file
    Co-authored-by: Jerrod Styrk <jstyrk@citadel.edu>

--- src/main.js
+++ src/main.js
@@ -2,103 +2,98 @@ import $ from 'jquery';
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './css/styles.css';
+import GifService from './js/gif-service.js';
+
+function getElements(body) {
+  $('.showGif1').show();
+  $('.showGif1').html(`<img src=${body.data[0].images.original.url}>`);
+  $('.showGif2').show();
+  $('.showGif2').html(`<img src=${body.data[1].images.original.url}>`);
+  $('.showGif3').show();
+  $('.showGif3').html(`<img src=${body.data[2].images.original.url}>`);
+}
 
 $(document).ready(function() {
   $('#searchButton').click (function() {
     const lookup = $('#searchBar').val();
     $('#searchBar').val("");
-    console.log(lookup);
-
-    let request = new XMLHttpRequest();
-    const url = `https://api.giphy.com/v1/gifs/search?api_key=${process.env.API_KEY}&q=${lookup}&limit=3&offset=0&rating=g&lang=en`;
-
-    request.onreadystatechange = function () {
-      if (this.readyState === 4 && this.status ===200) {
-        const response = JSON.parse(this.responseText);
-        getElements(response);
-      }
-    };
-    
-    request.open("GET", url, true);
-    request.send();
-
-    function getElements(response) {
+    let promise = GifService.getGif(lookup);
+    promise.then(function(response) {
+      const body = JSON.parse(response);
+      getElements(body);
+    }, function(error) {
+      $('.showGif1').text(`There was an error Processing this request: ${error}`);
       $('.showGif1').show();
-      $('.showGif1').html(`<img src=${response.data[0].images.original.url}>`);
-      $('.showGif2').show();
-      $('.showGif2').html(`<img src=${response.data[1].images.original.url}>`);
-      $('.showGif3').show();
-      $('.showGif3').html(`<img src=${response.data[2].images.original.url}>`);
-    }
+    });
   });
-  $('#trendingButton').click (function() {
+//   $('#trendingButton').click (function() {
     
 
-    let request = new XMLHttpRequest();
-    const url = `https://api.giphy.com/v1/gifs/trending?api_key=${process.env.API_KEY}&limit=3&rating=g`;
+//     let request = new XMLHttpRequest();
+//     const url = `https://api.giphy.com/v1/gifs/trending?api_key=${process.env.API_KEY}&limit=3&rating=g`;
 
-    request.onreadystatechange = function () {
-      if (this.readyState === 4 && this.status ===200) {
-        const response = JSON.parse(this.responseText);
-        getElements(response);
-      }
-    };
+//     request.onreadystatechange = function () {
+//       if (this.readyState === 4 && this.status ===200) {
+//         const response = JSON.parse(this.responseText);
+//         getElements(response);
+//       }
+//     };
     
-    request.open("GET", url, true);
-    request.send();
+//     request.open("GET", url, true);
+//     request.send();
 
-    function getElements(response) {
-      $('.showGif1').show();
-      $('.showGif1').html(`<img src=${response.data[0].images.original.url}>`);
-      $('.showGif2').show();
-      $('.showGif2').html(`<img src=${response.data[1].images.original.url}>`);
-      $('.showGif3').show();
-      $('.showGif3').html(`<img src=${response.data[2].images.original.url}>`);
-    }
+//     function getElements(response) {
+//       $('.showGif1').show();
+//       $('.showGif1').html(`<img src=${response.data[0].images.original.url}>`);
+//       $('.showGif2').show();
+//       $('.showGif2').html(`<img src=${response.data[1].images.original.url}>`);
+//       $('.showGif3').show();
+//       $('.showGif3').html(`<img src=${response.data[2].images.original.url}>`);
+//     }
 
-  });
+//   });
 
-  $('#randomButton').click (function() {
-    const tags = $('tags').val();
-    const source = $('sourceUrl').val();
+//   $('#randomButton').click (function() {
+//     const tags = $('tags').val();
+//     const source = $('sourceUrl').val();
 
-    let request = new XMLHttpRequest();
-    const url = `https://api.giphy.com/v1/gifs/random?api_key=${process.env.API_KEY}&source_image_url=${source}&tag=${tags}`;
+//     let request = new XMLHttpRequest();
+//     const url = `https://api.giphy.com/v1/gifs/random?api_key=${process.env.API_KEY}&source_image_url=${source}&tag=${tags}`;
 
-    request.onreadystatechange = function () {
-      if (this.readyState === 4 && this.status ===200) {
-        const response = JSON.parse(this.responseText);
-        getElements(response);
-      }
-    };
+//     request.onreadystatechange = function () {
+//       if (this.readyState === 4 && this.status ===200) {
+//         const response = JSON.parse(this.responseText);
+//         getElements(response);
+//       }
+//     };
     
-    request.open("GET", url, true);
-    request.send();
+//     request.open("GET", url, true);
+//     request.send();
 
-    function getElements(response) {
-      $('.showGif1').show();
-      $('.showGif1').html(`<img src=${response.data.images.original.url}>`);
-    }
-  });
-  $('#uploadButton').click (function() {
+//     function getElements(response) {
+//       $('.showGif1').show();
+//       $('.showGif1').html(`<img src=${response.data.images.original.url}>`);
+//     }
+//   });
+//   $('#uploadButton').click (function() {
     
 
-    let request = new XMLHttpRequest();
-    const url = `upload.giphy.com/v1/gifs/api_key=${process.env.API_KEY}&tag=`;
+//     let request = new XMLHttpRequest();
+//     const url = `upload.giphy.com/v1/gifs/api_key=${process.env.API_KEY}&tag=`;
 
-    request.onreadystatechange = function () {
-      if (this.readyState === 4 && this.status ===200) {
-        const response = JSON.parse(this.responseText);
-        getElements(response);
-      }
-    };
+//     request.onreadystatechange = function () {
+//       if (this.readyState === 4 && this.status ===200) {
+//         const response = JSON.parse(this.responseText);
+//         getElements(response);
+//       }
+//     };
     
-    request.open("POST", url, true);
-    request.send();
+//     request.open("POST", url, true);
+//     request.send();
 
-    function getElements(response) {
-      $('.showGif1').show();
-      $('.showGif1').html(`<img src=${response.data.images.original.url}>`);
-    }
-  });
+//     function getElements(response) {
+//       $('.showGif1').show();
+//       $('.showGif1').html(`<img src=${response.data.images.original.url}>`);
+//     }
+//   });
 });
